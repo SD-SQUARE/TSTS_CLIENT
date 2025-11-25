4758a92a5bb3338220368906b93fb36dad69e950
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-11-25 10:32:13 +0200

    [add]: base path to env and config

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,9 +2,12 @@
 export const API_HOST = import.meta.env.VITE_API_HOST;
 export const API_PORT = import.meta.env.VITE_API_PORT;
 export const API_PROTOCOL = import.meta.env.VITE_API_PROTOCOL;
+export const API_BASE_PATH = import.meta.env.VITE_API_BASE_PATH;
+
 // app settings
 export const APP_HOST = import.meta.env.VITE_APP_HOST;
 export const APP_PORT = import.meta.env.VITE_APP_PORT;
 export const APP_PROTOCOL = import.meta.env.VITE_APP_PROTOCOL;
+export const APP_BASE_PATH = import.meta.env.VITE_APP_BASE_PATH;
 // language
 export const DEFAULT_LANGUAGE = import.meta.env.VITE_DEFAULT_LANGUAGE ?? 'en'
