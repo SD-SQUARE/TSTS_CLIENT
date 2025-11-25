918e2c4f36cd57a427a4244566ba3e35324c7c3e
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-11-25 15:34:21 +0200

    [add]: fallback values for envs

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,13 +1,14 @@
 // api settings
-export const API_HOST = import.meta.env.VITE_API_HOST;
-export const API_PORT = import.meta.env.VITE_API_PORT;
-export const API_PROTOCOL = import.meta.env.VITE_API_PROTOCOL;
-export const API_BASE_PATH = import.meta.env.VITE_API_BASE_PATH;
+export const API_HOST = import.meta.env.VITE_API_HOST ?? "localhost";
+export const API_PORT = import.meta.env.VITE_API_PORT ?? 3000;
+export const API_PROTOCOL = import.meta.env.VITE_API_PROTOCOL ?? "http";
+export const API_BASE_PATH = import.meta.env.VITE_API_BASE_PATH ?? "api";
 
 // app settings
-export const APP_HOST = import.meta.env.VITE_APP_HOST;
-export const APP_PORT = import.meta.env.VITE_APP_PORT;
-export const APP_PROTOCOL = import.meta.env.VITE_APP_PROTOCOL;
-export const APP_BASE_PATH = import.meta.env.VITE_APP_BASE_PATH;
+export const APP_HOST = import.meta.env.VITE_APP_HOST ?? "localhost";
+export const APP_PORT = import.meta.env.VITE_APP_PORT ?? 5000;
+export const APP_PROTOCOL = import.meta.env.VITE_APP_PROTOCOL ?? "http";
+export const APP_BASE_PATH = import.meta.env.VITE_APP_BASE_PATH ?? "";
+
 // language
 export const DEFAULT_LANGUAGE = import.meta.env.VITE_DEFAULT_LANGUAGE ?? 'en'
