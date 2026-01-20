f41938fc1788b2aae65d0309cefda84853c58003
Habiba <habiba.elshaer004@gmail.com>
2026-01-20 17:19:12 +0200

    [update]: add chat reducer to store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/login/store/authSlice'
+import { chatApi } from '../features/tickets/store/services/chatApi'
 
 
 export const store = configureStore({
-    reducer: { auth: authReducer },
+    reducer: { 
+        [chatApi.reducerPath]: chatApi.reducer,
+        auth: authReducer },
+
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(chatApi.middleware),
 })
 
 
