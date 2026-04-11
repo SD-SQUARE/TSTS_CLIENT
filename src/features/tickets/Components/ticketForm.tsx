24483194feb0a392ddf8a501b051f315d28b172d
Habiba <habiba.elshaer004@gmail.com>
2026-04-11 17:48:17 +0200

    [fix]: problem type is not required in the ticket create form

--- src/features/tickets/Components/ticketForm.tsx
+++ src/features/tickets/Components/ticketForm.tsx
@@ -549,7 +549,7 @@ const TicketForm: React.FC = () => {
                             <Form.Item
                                 name="problem"
                                 noStyle
-                                rules={[{ required: true, message: t('errors.problemRequired') }]}
+                                // rules={[{ required: true, message: t('errors.problemRequired') }]}
                             >
                                 <Input type="hidden" />
                             </Form.Item>
