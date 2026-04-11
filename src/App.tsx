ca897ccaaaac533f08c092644559a043e7741171
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2026-04-11 07:23:15 +0200

    [fix]: navbar missing auth role

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,9 @@ function App() {
                         : (
                         
                         < >
-                            <NavItem to={`${APP_BASE_PATH}/dashboard`}>{t('DASHBOARD')}</NavItem>
+                            <GuardedRoute roles={["admin"]} allowNavigation={false}>
+                                <NavItem to={`${APP_BASE_PATH}/dashboard`}>{t('DASHBOARD')}</NavItem>
+                            </GuardedRoute>    
                             <NavItem to={`${APP_BASE_PATH}/knowledge-base`}>{t('Knowledge-Base')}</NavItem>
                             <NavItem to={`/${user.role.toLowerCase()}/tickets`}>{t('Tickets')}</NavItem>
                             <GuardedRoute roles={["admin"]} allowNavigation={false}>
